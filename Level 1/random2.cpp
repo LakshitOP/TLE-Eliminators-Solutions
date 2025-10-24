1fc254d0da47f431852936df57e1b3ff4f832501
LakshitOP <derxqq@gmail.com>
2025-10-24 09:57:19 +0530

    2nd Problem

---
+++ Level 1/random2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> nums = {a, b, c};
+    vector<int> kopy = nums;
+    sort(nums.begin(), nums.end());
+    for (int num : nums) {
+        cout << num <<endl;
+    }
+    cout << endl;
+    for (int op : kopy) {
+        cout << op <<endl;
+    }
+    return 0;
+}
---
+++ Level 1/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "" << endl;
+    cout << 10%3 << " "<< " " << 10/3;
+    
+    return 0;
+}
