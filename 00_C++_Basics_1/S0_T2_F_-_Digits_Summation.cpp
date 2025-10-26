240848de57c86e8bc74bd41c8277e965ec7f0ac7
lakshit mittal <derxqq@gmail.com>
2025-10-26 16:17:38 +0530

    cool

---
+++ 00_C++_Basics_1/S0_T2_F_-_Digits_Summation.cpp
@@ -0,0 +1,11 @@
+// Task: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >>a>>b;
+    cout << a%10 + b%10; 
+    return 0;
+}
