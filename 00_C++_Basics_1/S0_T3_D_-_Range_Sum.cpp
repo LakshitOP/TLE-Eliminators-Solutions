5d34830c4e48fa9f3a6cee84c06aa98e7e8e5fcb
lakshit mittal <derxqq@gmail.com>
2025-10-26 17:09:49 +0530

    Refactor S0_T3_D_-_Range_Sum.cpp: Add input handling and calculation for range sum

--- 00_C++_Basics_1/S0_T3_D_-_Range_Sum.cpp
+++ 00_C++_Basics_1/S0_T3_D_-_Range_Sum.cpp
@@ -1,9 +1,25 @@
 // Task: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/D
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+using ll = long long;
 
 int main() {
+    int n;
+    cin >> n;
+    while(n--) {
+        ll a, b; 
+        cin >> a >> b;
+        if (a > b) {
+            swap(a, b);
+        }
+
+        ll sum = (b - a + 1) * (a + b) / 2;
+        
+        
+        cout << sum << endl;
+    }
     
     return 0;
 }
