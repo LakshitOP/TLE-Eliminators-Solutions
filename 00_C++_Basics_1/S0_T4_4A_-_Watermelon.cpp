30f577c6e3bf2f5bb877bb38475b3df14f1fe887
lakshit mittal <derxqq@gmail.com>
2025-10-28 08:38:17 +0530

    Add initial implementation of S0_T4_4A_-_Watermelon.cpp for problem 4A

---
+++ 00_C++_Basics_1/S0_T4_4A_-_Watermelon.cpp
@@ -0,0 +1,12 @@
+// Task: https://codeforces.com/problemset/problem/4/A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;cin >> n;
+    if (n>2 && n%2==0) cout << "YES\n";
+    else cout << "NO\n";
+    
+    return 0;
+}
