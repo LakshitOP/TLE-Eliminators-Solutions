c25968b7e074ddf927dcd4ed94211bdd576360b3
lakshit mittal <derxqq@gmail.com>
2025-10-28 09:02:53 +0530

    nothung to commit, working tree clean

---
+++ 00_C++_Basics_1/S0_T5_G_-_Summation_from_1_to_N.cpp
@@ -0,0 +1,13 @@
+// Task: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main() {
+    ll n;
+    cin >> n;
+    cout << n * (n + 1) / 2 << endl;
+    
+    return 0;
+}
